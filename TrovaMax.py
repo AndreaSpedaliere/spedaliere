335baa888ea099996d16e2521e18b1cf00e1a29c
AndreaSpedaliere <Andreaspedaliere@gmail.com>
2021-11-21 20:19:47 +0100

    Create TrovaMax.py
    Trova il più grande di tre valori

---
+++ TrovaMax.py
@@ -0,0 +1,7 @@
+a = input("Inserisici primo valore: ")
+b = input("Inserisici secondo valore: ")
+c = input("Inserisici terzo valore: ")
+m = a
+if b > m: m = b
+if c > m: m = c
+print ("Il valore più grande è: " + m)
